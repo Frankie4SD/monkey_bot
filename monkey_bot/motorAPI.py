d743972d3f8a72bad594d4417d43c1839009a858
Colin Szeto <kolinszeto@gmail.com>
2025-12-06 18:24:46 -0800

    Update motorAPI.py
    to ensure positive corresponds with right

--- monkey_bot/motorAPI.py
+++ monkey_bot/motorAPI.py
@@ -32,7 +32,7 @@ class Movement():
         # mag = mangitude
         # positive is yawing right
         print("yaw")
-        self.send_to_arduino(255*mag,-255*mag, delay=time)
+        self.send_to_arduino(-255*mag,255*mag, delay=time)
 
     def stop(self,time):
         # mag = mangitude
